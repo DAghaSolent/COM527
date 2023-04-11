789aa711b253962a6c01f31dc5fb886b4bd31f22
Danny Agha <91552769+DAghaSolent@users.noreply.github.com>
2023-04-11 14:28:36 +0100

    LiveData Activity with Local SQL Database

---
+++ roomappbasic/HitDao.kt
@@ -0,0 +1,27 @@
+package com.example.roomappbasic
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface HitDao {
+
+    @Query("SELECT * FROM hits WHERE id=:id")
+    fun getHitById(id: Long) : Hit
+
+    @Query("SELECT * FROM hits")
+    fun getAllHits(): LiveData<List<Hit>>
+
+    @Insert
+    suspend fun insert(hit: Hit) : Long
+
+    @Query("SELECT * FROM hits WHERE artist=:artist")
+    fun getHitsByArtist(artist: String) : List<Hit>
+
+    @Delete
+    suspend fun delete(hit: Hit)
+
+    @Update
+    suspend fun update(hit: Hit)
+}
+
